2b2bd7d62a180a76e32611ee341ba01f99739968
Ben Fedoruk <fedorukben@gmail.com>
2021-05-22 16:05:07 -0400

    Added social-algos.dta

--- code/pre.py
+++ code/pre.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import cln
 import glbl as g
 
+# df = pd.io.stata.read_stata('file.dta')
 g.d['misinfo-type'] = pd.read_csv('../data/misinfo-type.csv')
 
-#plt.show()
+# plt.show()
