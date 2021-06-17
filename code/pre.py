921f33f3929b922b186ce5b4e895f4ff61bf7779
benfedoruk <71652978+benfedoruk@users.noreply.github.com>
2021-06-16 22:37:40 -0400

    Reworked Parler.py file

--- code/pre.py
+++ code/pre.py
@@ -6,13 +6,21 @@ import seaborn as sns
 import glbl as g
 import cln
 
+"""
 
+g.d = {
+    '4chan': pd.read_pickle("../data/pickle/4chan.pkl"),
+    'twitter': pd.read_pickle("../data/pickle/twitter.pkl"),
+}
+
+"""
 
 # df = pd.io.stata.read_stata('file.dta')
 g.d['misinfo-type'] = pd.read_pickle("../data/pickle/misinfo-type.pkl")
 g.d['twitter'] = pd.read_pickle('../data/pickle/twitter.pkl')
 g.d['reddit'] = pd.read_pickle('../data/pickle/reddit.pkl')
 g.d['4chan'] = pd.read_pickle('../data/pickle/4chan.pkl')
+g.d['parler-kw'] = pd.read_pickle('../data/pickle/Parler-name-filter.pkl')
 for sub in g.reddit_subs:
     g.d[f'reddit-{sub}'] = pd.read_pickle(f"../data/pickle/reddit-{sub}.pkl")
 for board in g.fchan_boards:
@@ -24,13 +32,14 @@ for sub in g.reddit_subs:
     cln.remove_zero_compound(f'reddit-{sub}')
 print("Data successfully cleaned.")
 
+
 #fig = sns.histplot(data=g.d['twitter'], x='compound', bins=50, kde=True, hue='neg')
 #fig = sns.scatterplot(data=g.d['twitter'], x='compound', y='words')
-fig = sns.histplot(data=g.d['4chan'], x='compound', bins=25, kde=True)
-#fig = sns.boxplot(g.d['reddit']['compound'])
+fig = sns.histplot(data=g.d['twitter'], x='compound', bins=25, kde=True)
+# #fig = sns.boxplot(g.d['reddit']['compound'])
 fig.set(xlabel='compounded positivity score on [-1, 1]', 
         ylabel='frequency', 
         #title='Reddit r/movies Positivity vs. No. of Words',)
-        title='INSERT THING HERE Frequency of Positivity')
+        title='Parler Frequency of Positivity by Keyword')
 print("Plotting figure...")
 plt.show()
