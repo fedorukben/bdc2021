836633fe3dd8e4bd17a5552e69bc899035f133c6
Kai <kladouceur06@gmail.com>
2021-06-18 22:09:47 -0400

    bad and good filter for Parler finished

--- code/Twitterkeyterm.py
+++ code/Twitterkeyterm.py
@@ -9,17 +9,26 @@ sia = SentimentIntensityAnalyzer()
 
 pbar = ProgressBar()
 
-df = pd.read_pickle(r"C:\Users\Kai Fucile Ladouceur\.vscode\Code\bdc2021\data\pickle\twitter.pkl")
+df = pd.read_pickle(r"C:\Users\Kai Fucile Ladouceur\.vscode\Code\bdc2021\data\pickle\parley.pkl")
 # print(df['headline'][278].tolist()[-1])
 # number_of_entries = df.shape[0] - 5 # subtracted 5 to deal with index errors
-df2 = pd.DataFrame()
-key_terms = ["china flu","wuflu","china virus","wuhan virus", "kungflu","wuhan","lab leak"]
+key_terms = ["sars-cov-2","severe acute respiratory syndrome coronavirus 2"]
+
+df["headline"] = df['headline'].str.lower()
+df.reset_index()
+#test out seeing if using sars-cov-2 and scientific names have reduced negative sentiment when compared to just using covid or covid 19
+
+df3 = pd.DataFrame()
+# print(df)
 for index, harrisonisthebest in df.iterrows():
-# if "variant" in harrisonisthebest['text'].lower():
-  #   for keyterm in ["british", "indian","uk","brazil","britain","u.k.","india","south africa","brazilian","african"]:
-  for keyterm in key_terms:
-    if keyterm in harrisonisthebest['text'].lower():
-      df2 = pd.concat([df2, harrisonisthebest.to_frame().T])
+   for keyterm in key_terms:
+    if keyterm in harrisonisthebest['headline'].lower():
+      df3 = pd.concat([df3, harrisonisthebest.to_frame().T])
 
-
-print(df2)
+print(df3)
+df3['compound'] = pd.to_numeric(df3['compound'])
+df3['pos'] = pd.to_numeric(df3['pos'])
+df3['neg'] = pd.to_numeric(df3['neg'])
+df3['neu'] = pd.to_numeric(df3['neu'])
+df3['label'] = pd.to_numeric(df3['label'])
+# df3.to_pickle(r"C:\Users\Kai Fucile Ladouceur\.vscode\Code\bdc2021\data\pickle\Parler-name-filter.pkl")
