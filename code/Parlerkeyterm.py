7a324543bbfd6dc72a6ec58d44276925e9da5f83
Kai <kladouceur06@gmail.com>
2021-06-18 21:35:50 -0400

    Filtered parler name data

--- code/Parlerkeyterm.py
+++ code/Parlerkeyterm.py
@@ -16,12 +16,20 @@ key_terms = ["china flu","wuflu","china virus","wuhan virus", "kungflu","wuhan",
 
 df["text"] = df['text'].str.lower()
 df.reset_index()
+#test out seeing if using sars-cov-2 and scientific names have reduced negative sentiment when compared to just using covid or covid 19
+
+df3 = pd.DataFrame()
 
 for index, harrisonisthebest in df.iterrows():
-  # if "variant" in harrisonisthebest['text'].lower():
-  #   for keyterm in ["british", "indian","uk","brazil","britain","u.k.","india","south africa","brazilian","african"]:
-  for keyterm in key_terms:
-    if keyterm not in harrisonisthebest["text"]: 
-      df.drop(index)
+   for keyterm in key_terms:
+    if keyterm in harrisonisthebest['text'].lower():
+      df3 = pd.concat([df3, harrisonisthebest.to_frame().T])
 
-df.to_pickle("Parler-name-filter.pkl")
+print(df3)
+df3['compound'] = pd.to_numeric(df3['compound'])
+df3['pos'] = pd.to_numeric(df3['pos'])
+df3['neg'] = pd.to_numeric(df3['neg'])
+df3['neu'] = pd.to_numeric(df3['neu'])
+df3['label'] = pd.to_numeric(df3['label'])
+print(df3.info())
+df.to_pickle(r"C:\Users\Kai Fucile Ladouceur\.vscode\Code\bdc2021\data\pickle\Parler-name-filter.pkl")
