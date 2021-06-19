109625c19677038ca949228cce5913c8f28634a0
Kai <kladouceur06@gmail.com>
2021-06-18 21:52:37 -0400

    New filter

--- code/Test.py
+++ code/Test.py
@@ -1,4 +1,4 @@
 import pandas as pd 
 
-df = pd.read_pickle(r'C:\Users\Kai Fucile Ladouceur\.vscode\Code\bdc2021\data\pickle\4chan-all.pkl')
+df = pd.read_pickle(r'C:\Users\Kai Fucile Ladouceur\.vscode\Code\bdc2021\data\pickle\Parler-name-filter.pkl')
 print(df.head())
